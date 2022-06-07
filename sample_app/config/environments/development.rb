f299eb63677c1e7ab43b28f35ed479c94138d95b
OumeiSatoKenta <user.email>
2022-06-07 15:03:44 +0000

    sample_app: helloを追加した

--- sample_app/config/environments/development.rb
+++ sample_app/config/environments/development.rb
@@ -55,6 +55,8 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+  # Cloud9への接続を許可する
+  config.hosts.clear
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
